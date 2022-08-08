0687172a52b2e49c21a1fd1be2c8ef8d1128aade
mndiaz90 <mayler900123@gmail.com>
2022-08-08 18:14:49 +0200

    Add BarChart component inside ProductCard

--- src/components/ProductCard/index.js
+++ src/components/ProductCard/index.js
@@ -1,3 +1,4 @@
+import BarChart from "../BarChart";
 import "./styles.css";
 
 const ProductCard = ({ code, name, price, wh_coverage, size_stock, count }) => {
@@ -13,6 +14,19 @@ const ProductCard = ({ code, name, price, wh_coverage, size_stock, count }) => {
         <span className="card-data__code">{code}</span>
         <span className="card-data__name">{name}</span>
         <span className="card-data__price">{price} &euro;</span>
+        <div className="card-data__chart">
+          <BarChart
+            chartData={{
+              labels: Object.keys(size_stock),
+              datasets: [
+                {
+                  data: Object.values(size_stock),
+                  backgroundColor: "black",
+                },
+              ],
+            }}
+          />
+        </div>
         <span>{wh_coverage}</span>
         {wh_coverage < 0.5 ? (
           <span className="card-data__coverage-low">Very Low</span>
