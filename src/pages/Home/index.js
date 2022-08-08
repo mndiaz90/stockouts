bc78733efd63bf75d6b31d059b5e8595869a9312
mndiaz90 <mayler900123@gmail.com>
2022-08-08 13:23:26 +0200

    Add header to Home page

---
+++ src/pages/Home/index.js
@@ -0,0 +1,12 @@
+const Home = () => {
+  return (
+    <>
+      <header className="App-header">
+        <span>Top Stockouts Review</span>
+      </header>
+      <main></main>
+    </>
+  );
+};
+
+export default Home;
