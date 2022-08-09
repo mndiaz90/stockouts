1dd44115c3260a7ca00c81ce5a882e733a88eb53
mndiaz90 <mayler900123@gmail.com>
2022-08-09 16:25:59 +0200

    Add styles to ProgressBar component

--- src/components/ProgressBar/index.js
+++ src/components/ProgressBar/index.js
@@ -10,8 +10,8 @@ const ProgressBar = ({ bgcolor = "green", completed }) => {
   return (
     <div className="progress-bar">
       <span className="progress-bar__whcoverage">{completed}</span>
-      <span>%</span>
-      <span>STOCKOUT</span>
+      <span className="progress-bar__percent">%</span>
+      <span className="progress-bar__stockout">STOCKOUT</span>
       <div className="progress-bar__div progress-bar__div-empty"></div>
       <div className="progress-bar__div" style={progressBarFilled}></div>
     </div>
