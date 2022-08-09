ff7550f45ae0e459c2d797cc49fb45b6f9953203
mndiaz90 <mayler900123@gmail.com>
2022-08-09 23:16:17 +0200

    Move ProductList component to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import Home from "./pages/Home";
+import { useEffect, useState } from "react";
+import ProductsList from "./components/ProductsList";
+import products from "./data/products.json";
+import sortProductsByCoverageAndRanking from "./utils/sortProducts";
 import "./App.css";
 
 function App() {
+  const [sortedProducts, setSortedProducts] = useState([]);
+
+  useEffect(() => {
+    const sortedProductsBy = sortProductsByCoverageAndRanking(products);
+    setSortedProducts(sortedProductsBy);
+  }, []);
   return (
     <div className="App">
-      <Home />
+      <header className="App-header">
+        <span>Top Stockouts Review</span>
+      </header>
+      <main>
+        <ProductsList
+          sortedProducts={sortedProducts}
+          setSortedProducts={setSortedProducts}
+        />
+      </main>
     </div>
   );
 }
--- src/components/ProductsList/index.js
+++ src/components/ProductsList/index.js
@@ -1,17 +1,7 @@
-import { useEffect, useState } from "react";
-import products from "../../data/products.json";
-import sortProductsByCoverageAndRanking from "../../utils/sortProducts";
 import ProductCard from "../ProductCard";
 import "./styles.css";
 
-const ProductsList = () => {
-  const [sortedProducts, setSortedProducts] = useState([]);
-
-  useEffect(() => {
-    const sortedProductsBy = sortProductsByCoverageAndRanking(products);
-    setSortedProducts(sortedProductsBy);
-  }, []);
-
+const ProductsList = ({ sortedProducts, setSortedProducts }) => {
   const removeProduct = (code) => {
     const products = sortedProducts.filter((product) => product.code !== code);
     setSortedProducts(products);
