b0f87b66a902c6cfd408561b8a876894c696a565
mndiaz90 <mayler900123@gmail.com>
2022-08-10 15:46:19 +0200

    Fix sort function implementation

--- src/utils/sortProducts.js
+++ src/utils/sortProducts.js
@@ -1,5 +1,5 @@
 export default function sortProductsByCoverageAndRanking(products = []) {
   return products.sort((a, b) => {
-    return a.wh_coverage - b.wh_coverage || b.sales_ranking - a.sales_ranking;
+    return a.wh_coverage - b.wh_coverage || a.sales_ranking - b.sales_ranking;
   });
 }
