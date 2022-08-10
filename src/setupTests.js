de2ed9dd1f66ac9937124de5d92c77d7e0c76fdd
mndiaz90 <mayler900123@gmail.com>
2022-08-10 13:42:25 +0200

    Setup jest to handle canvas

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,11 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+window.ResizeObserver = function () {
+  return {
+    observe: jest.fn(),
+    unobserve: jest.fn(),
+    disconnect: jest.fn(),
+  };
+};
