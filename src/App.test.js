4df8c7adfe389248bd762323cdccfd7520777d29
mndiaz90 <mayler900123@gmail.com>
2022-08-11 11:37:26 +0200

    Remove unused async from tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,14 +2,14 @@ import { screen, render } from "@testing-library/react";
 import App from "./App";
 
 describe("<App />", () => {
-  it("should display the header text", async () => {
+  it("should display the header text", () => {
     render(<App />);
 
     expect(screen.getByRole("banner")).toBeInTheDocument();
     expect(screen.getByText("Top Stockouts Review")).toBeInTheDocument();
   });
 
-  it("should display the list of products", async () => {
+  it("should display the list of products", () => {
     render(<App />);
 
     expect(screen.getByRole("list")).toBeInTheDocument();
